6cfaee63a40558de30b2f5d29a8540cd01626e94
2ndyrGroupA <2ndyrGroupA@pn.local>
2019-09-28 19:31:15 +0800

    Premid Number2

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Pm_number2 from './pm_number2';
+
+function App() {
+  return (
+    <div>
+      <Pm_number2/>
+    </div>
+  );
+  // <div className="App">
+  //   <header className="App-header">
+  //     <img src={logo} className="App-logo" alt="logo" />
+  //     <p>
+  //       Edit <code>src/App.js</code> and save to reload.
+  //     </p>
+  //     <a
+  //       className="App-link"
+  //       href="https://reactjs.org"
+  //       target="_blank"
+  //       rel="noopener noreferrer"
+  //     >
+  //       Learn React
+  //     </a>
+  //   </header>
+  // </div>
+}
+export default App;
